c896a1f7fdb53979a47cd57687dd051b86d6b6dc
Rachele-Turtora <142145337+Rachele-Turtora@users.noreply.github.com>
2024-07-08 14:31:39 +0200

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel Template</title>
+    @vite('resources/js/app.js')
+</head>
+
+<body>
+    <h1>Template Laravel</h1>
+
+    <img src="{{ Vite::asset('resources/img/pikachu.png') }}" alt="">
+</body>
+
+</html>
